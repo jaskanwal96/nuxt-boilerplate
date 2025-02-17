4cbd08a4676f085f09e6467f0b613396ccbf0ea6
Supernova <jaskanwal196@gmail.com>
2025-02-18 00:29:38 +0900

    Boiler Plate Code - Init

---
+++ components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        Hello World
+    </div>
+</template>
+<style scoped>
+.container {
+    font-weight: bold;
+}
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,6 @@
+<template>
+    This is a test page {{  testVar }}
+</template>
+<script lang="ts" setup>
+const testVar = ref('Sunny')
+</script>
